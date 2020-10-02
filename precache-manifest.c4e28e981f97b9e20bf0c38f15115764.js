d856c9f8ec8f4cb63c811a7663ee3483f0e3740f
Andrew Hua <andrewhua90@gmail.com>
2020-10-02 17:53:58 -0400

    Updates

---
+++ precache-manifest.c4e28e981f97b9e20bf0c38f15115764.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bf9ea80e84cd03833b540d58cff3ea",
+    "url": "/andrew-web-app/index.html"
+  },
+  {
+    "revision": "9dcff95b2aff94ed8e98",
+    "url": "/andrew-web-app/static/css/2.beaa289f.chunk.css"
+  },
+  {
+    "revision": "9dcff95b2aff94ed8e98",
+    "url": "/andrew-web-app/static/js/2.d100f6d5.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/andrew-web-app/static/js/2.d100f6d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa69ccb5fef68dcb617",
+    "url": "/andrew-web-app/static/js/main.916ca57b.chunk.js"
+  },
+  {
+    "revision": "7d2b741c74e2f9d0f18a",
+    "url": "/andrew-web-app/static/js/runtime-main.de416a35.js"
+  },
+  {
+    "revision": "3ac5eea9cf6bd4ac0d0e2858199c9b13",
+    "url": "/andrew-web-app/static/media/lato-all-400-italic.3ac5eea9.woff"
+  },
+  {
+    "revision": "abe0a52a291f69ef321ae49f6f04fda9",
+    "url": "/andrew-web-app/static/media/lato-all-400-normal.abe0a52a.woff"
+  },
+  {
+    "revision": "117e995c97eab30fb92843616018d1f1",
+    "url": "/andrew-web-app/static/media/lato-latin-400-italic.117e995c.woff2"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/andrew-web-app/static/media/lato-latin-400-normal.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "3ffd3984b24adbab2573ff500daca12a",
+    "url": "/andrew-web-app/static/media/lato-latin-ext-400-italic.3ffd3984.woff2"
+  },
+  {
+    "revision": "3952106caf9b878ad83fc5be709d0d45",
+    "url": "/andrew-web-app/static/media/lato-latin-ext-400-normal.3952106c.woff2"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/andrew-web-app/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/andrew-web-app/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/andrew-web-app/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/andrew-web-app/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/andrew-web-app/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/andrew-web-app/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/andrew-web-app/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/andrew-web-app/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/andrew-web-app/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/andrew-web-app/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/andrew-web-app/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/andrew-web-app/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/andrew-web-app/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/andrew-web-app/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/andrew-web-app/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/andrew-web-app/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
